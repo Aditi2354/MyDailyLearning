1b4527e8423f50d6ed49d4854146552a8601e3e2
AditiKesharwani <hiaditii2001@gmail.com>
2026-04-24 18:26:23 +0530

    Import all resusable Components

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,24 @@ import Map from './Listmap'
 import { useEffect } from 'react'
 import { Outer } from './Closure'
 import Profile from './Profile'
+import Playing from './Props'
+import BOX from './Children'
+import IfandElse from './IfandElse'
+import StatusMessage from './SwitchCase'
+import ProductItems from './Items'
+import Authentication from './Authentication'
+import Update from './SetState'
+import Eventhandled from './Event'
+import Parent from './Parent'
+import User from './Conditional'
 function App() {
-  
+     let ProductList= ['shoes','perfume','jeans','top'] 
 const name ='Aditi'
 
 // const Welcome = (props)=>{
 //  return <div>{props.children}</div>
 // }
+
 useEffect(()=>{
 const closure =Outer()
 closure()
@@ -35,9 +46,49 @@ closure()
       <br></br>
       <Map/>
       <br></br>
-      <Profile name="Aditi" age = "24"/>
+      <Profile name='Aditi' age={24} />
      </div>
+     <>
+     <div>
+
+    <Playing game="badminton"/>
+     </div>
+     <br></br>
+     <div>
+      <BOX>
+        <p>This is what i am DON</p>
+      </BOX>
+     </div>
+     </>
+     <div>
+      <IfandElse isLoggedIn={false}/>
+     </div>
+
+     <div>
+      <StatusMessage status='Loading'/>
+     </div>
+      <div>
+       <ProductItems Items={ProductList}/>
+      </div>
+
+      <div>
+        <Authentication/>
+      </div>
+
+      <div>
+        <Update/>
+      </div>
+      <div>
+        <Eventhandled/>
+      </div>
+      <div>
+        <Parent/>
+      </div>
+      <div>
+        <User isLoggedIn={false} isAdmin={false} isBanned={true}/>
+      </div>
     </>
+    
   )
 }
 
